71d9327f05f234a7352e299b6764787bfc2174b6
AlexanderL <al.x.300000@gmail.com>
2022-07-03 18:05:04 -0400

    refactor

--- front/job-search/src/pages/fav.js
+++ front/job-search/src/pages/fav.js
@@ -8,9 +8,14 @@ const About = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "90vh",
+        fontSize: "30pt",
       }}
     >
-      <h1>About</h1>
+      <ul>
+        <li>fav1</li>
+        <li>fav2</li>
+        <li>to be added dynamically with array</li>
+      </ul>
     </div>
   );
 };
